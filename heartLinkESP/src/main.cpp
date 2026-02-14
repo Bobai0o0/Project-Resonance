bdcad134fc317ed7985904a0f0ed822c22853c12
Maksim59 <max.isayenka@gmail.com>
2026-02-14 15:44:58 -0500

    code for heart rate detection.

--- heartLinkESP/src/main.cpp
+++ heartLinkESP/src/main.cpp
@@ -1,18 +1,29 @@
+//#include "VectorHaptics.h"
+//#include "ESP32Profiles.h"
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+/*VectorHaptics vh;
+VHChannels channels;
+ESP32Profile myBoard;*/
+#define heartPin 2
+int heartVal = 0;
+// JUMPER CABLE ON IO0  
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    pinMode(heartPin, INPUT);
+    /*
+    channels.add(1, {25});       // Channel 1 on GPIO pin 25
+    vh.init(&myBoard, {&channels});
+    */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  heartVal = analogRead(heartPin);
+  Serial.println(heartVal);
+  delay(1000);
+  /*
+    vh.play({PULSE(20, 1, 1)});  // Short strong pulse
+    delay(1000);                  // Wait 1 second
+    */
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
